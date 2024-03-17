bd9e0a6dcfd5ea45381674457fa7d6adf935b485
Steven Castiblanco <50757337+ztevenx100@users.noreply.github.com>
2024-03-17 00:10:10 -0500

    TASK: Adicionar campo estado en la opcion cliente

--- empresa_gestion_logistica/src/main/java/com/ingeneotest/empresa_gestion_logistica/models/Cliente.java
+++ empresa_gestion_logistica/src/main/java/com/ingeneotest/empresa_gestion_logistica/models/Cliente.java
@@ -23,17 +23,20 @@ public class Cliente {
     private String email;
     @Column(name = "cli_direccion")
     private String direccion;
+    @Column(name = "cli_estado")
+    private String estado;
     
     public Cliente() {
     }
     
-    public Cliente(String id, String tipoId, String nombre, String telefono, String email, String direccion) {
+    public Cliente(String id, String tipoId, String nombre, String telefono, String email, String direccion, String estado) {
         this.id = id;
         this.tipoId = tipoId;
         this.nombre = nombre;
         this.telefono = telefono;
         this.email = email;
         this.direccion = direccion;
+        this.estado = estado;
     }
     
     public String getId() {
@@ -76,6 +79,14 @@ public class Cliente {
     }
     public void setDireccion(String direccion) {
         this.direccion = direccion;
+    }
+    
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
     }
     
     @Override
