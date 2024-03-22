8724e7641599343ed944912a45507232d80c5814
Pedro Steven Castiblanco <pcastiblanco@megasoft.com.co>
2024-03-21 22:57:21 -0500

    TASK: Adicionar Toast de mensaje de notifcacion y error en las opciones - [Transporte] Ajustar error de tipo - [Transporte] Adicionar toast

--- empresa_gestion_logistica/src/main/java/com/ingeneotest/empresa_gestion_logistica/controllers/TransporteController.java
+++ empresa_gestion_logistica/src/main/java/com/ingeneotest/empresa_gestion_logistica/controllers/TransporteController.java
@@ -11,9 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ingeneotest.empresa_gestion_logistica.factory.TransporteFactory;
 import com.ingeneotest.empresa_gestion_logistica.factory.TransporteMaritimoFactory;
+import com.ingeneotest.empresa_gestion_logistica.factory.TransporteTerrestreFactory;
 import com.ingeneotest.empresa_gestion_logistica.models.Transporte;
 import com.ingeneotest.empresa_gestion_logistica.models.TransporteInterface;
 import com.ingeneotest.empresa_gestion_logistica.models.TransporteMaritimo;
@@ -34,12 +36,15 @@ public class TransporteController implements TransporteControllerInterface{
     @GetMapping("/transporte")
     public String obtenerTodosTransportes(Model model) {
         String path = "transporte/transporteLst";
+        System.out.println("get - obtenerTodosTransportes ");
 
         try {
             List<Transporte> l = transporteService.obtenerTodosTransportes();
             model.addAttribute("transportes", l);
             TransporteInterface vo = new Transporte();
             model.addAttribute("transporteNuevo", vo);
+            model.addAttribute("notificacion", model.getAttribute("notificacion"));
+            model.addAttribute("error", model.getAttribute("error"));
         } catch (Exception e) {
             path = "error";
             System.out.println(e);
@@ -50,10 +55,10 @@ public class TransporteController implements TransporteControllerInterface{
 
     @Override
     @GetMapping("/transporte/{action}/{id}")
-    public String obtenerTransportePorId(@PathVariable String action, @PathVariable("id") String id, Model model) {
+    public String obtenerTransportePorId(@PathVariable("action") String action, @PathVariable("id") String id, Model model) {
         Optional<Transporte> Transporte = null;
         String path = "transporte/transporteAdm";
-        System.out.println("get - guardarTransporte - id: " + id);
+        System.out.println("get - obtenerTransportePorId - id: " + id);
         try {
             if (!"0".equalsIgnoreCase(id)) {
                 Transporte = transporteService.obtenerTransportePorId(id);
@@ -65,6 +70,9 @@ public class TransporteController implements TransporteControllerInterface{
             }
             model.addAttribute("transporte", Transporte.get());
             model.addAttribute("accion", action);
+            //System.out.println("notificacion " + model.getAttribute("notificacion"));
+            model.addAttribute("notificacion", model.getAttribute("notificacion"));
+            model.addAttribute("error", model.getAttribute("error"));
         } catch (Exception e) {
             path = "error";
             System.out.println(e);
@@ -75,10 +83,10 @@ public class TransporteController implements TransporteControllerInterface{
     
     @Override
     @PostMapping("/transporte/{action}/{id}")
-    public String guardarTransporte(@PathVariable String action, @PathVariable("id") String id, @ModelAttribute Transporte transporte, Model model) {
+    public String guardarTransporte(@PathVariable("action") String action, @PathVariable("id") String id, @ModelAttribute Transporte transporte, Model model, RedirectAttributes redirectAttributes) {
         String path = "transporte/transporteAdm";
         TransporteFactory factory = null;
-        System.out.println("post - guardarTransporte - id; " + id);
+        System.out.println("post - guardarTransporte - id: " + id + " - action: " + action);
         try {
             path = "redirect:/transporte/upd/" + transporte.getId();
 
@@ -87,15 +95,13 @@ public class TransporteController implements TransporteControllerInterface{
                 TransporteMaritimo transporteMaritimo = (TransporteMaritimo) factory.crearTransporte(transporte);
                 if (!transporteMaritimo.validarFormatoMatricula()){
                     System.err.println("El formato debe corresponder a 3 letras iniciales, seguidas de 4 números y finalizando con una letra");
-
                     return path;
                 }
             } else if (TransporteServiceInterface.TIPO_TERRESTRE.equalsIgnoreCase(transporte.getTipo()) ){
-                factory = new TransporteMaritimoFactory();
+                factory = new TransporteTerrestreFactory();
                 TransporteTerrestre transporteTerrestre = (TransporteTerrestre) factory.crearTransporte(transporte);
                 if (!transporteTerrestre.validarFormatoMatricula()){
                     System.err.println("El formato debe corresponder a 3 letras iniciales y 3 números finales");
-
                     return path;
                 }
             }
@@ -103,24 +109,28 @@ public class TransporteController implements TransporteControllerInterface{
             transporteService.guardarTransporte(transporte);
             //new ResponseEntity<>(nuevoTransporte, HttpStatus.CREATED)
             model.addAttribute("accion", "upd");
+            redirectAttributes.addFlashAttribute("notificacion", "Transporte " + (("add".equalsIgnoreCase(action))?"adicionado":"actualizado"));
         } catch (Exception e) {
             path = "error";
             System.out.println(e);
+            redirectAttributes.addFlashAttribute("error", "Error "  + (("add".equalsIgnoreCase(action))?"adicionado":"actualizado") + " transporte");
         }
         return path;
     }
 
     @Override
     @GetMapping("/transporte/eliminar/{id}")
-    public String eliminarTransporte(@PathVariable("id") String id, Model model) {
+    public String eliminarTransporte(@PathVariable("id") String id, Model model, RedirectAttributes redirectAttributes) {
         System.out.println("delete - eliminarTransporte - id; " + id);
         String path = "transporte/transporteLst";
         try {
-            transporteService.eliminarTransporte(id);
             path = "redirect:/transporte";
+            transporteService.eliminarTransporte(id);
+            redirectAttributes.addFlashAttribute("notificacion", "Transporte eliminado");
         } catch (Exception e) {
             path = "error";
             System.out.println(e);
+            redirectAttributes.addFlashAttribute("error", "Error eliminado transporte");
         }
         return path;
     }
--- empresa_gestion_logistica/src/main/java/com/ingeneotest/empresa_gestion_logistica/controllers/TransporteControllerInterface.java
+++ empresa_gestion_logistica/src/main/java/com/ingeneotest/empresa_gestion_logistica/controllers/TransporteControllerInterface.java
@@ -1,6 +1,7 @@
 package com.ingeneotest.empresa_gestion_logistica.controllers;
 
 import org.springframework.ui.Model;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ingeneotest.empresa_gestion_logistica.models.Transporte;
 
@@ -10,8 +11,8 @@ public interface TransporteControllerInterface {
 
     String obtenerTransportePorId(String action, String id, Model model);
 
-    String guardarTransporte(String action, String id, Transporte transporte, Model model);
+    String guardarTransporte(String action, String id, Transporte transporte, Model model, RedirectAttributes redirectAttributes);
 
-    String eliminarTransporte(String id, Model model);
+    String eliminarTransporte(String id, Model model, RedirectAttributes redirectAttributes);
 
 }
