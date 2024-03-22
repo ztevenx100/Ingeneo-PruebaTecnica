cbca068e72cfb05a8c54a88e8f7c8e4ce15b985b
Pedro Steven Castiblanco <pcastiblanco@megasoft.com.co>
2024-03-22 15:44:52 -0500

    TASK: adicionar notificaciones toast a la opcion Clientes

--- empresa_gestion_logistica/src/main/java/com/ingeneotest/empresa_gestion_logistica/controllers/ClienteControllerInterface.java
+++ empresa_gestion_logistica/src/main/java/com/ingeneotest/empresa_gestion_logistica/controllers/ClienteControllerInterface.java
@@ -1,6 +1,7 @@
 package com.ingeneotest.empresa_gestion_logistica.controllers;
 
 import org.springframework.ui.Model;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ingeneotest.empresa_gestion_logistica.models.Cliente;
 
@@ -10,8 +11,8 @@ public interface ClienteControllerInterface {
 
     String obtenerClientePorId(String action, String id, Model model);
 
-    String guardarCliente(String action, String id, Cliente cliente, Model model);
+    String guardarCliente(String action, String id, Cliente cliente, Model model, RedirectAttributes redirectAttributes);
 
-    String eliminarCliente(String id, Model model);
+    String eliminarCliente(String id, Model model, RedirectAttributes redirectAttributes);
 
 }
